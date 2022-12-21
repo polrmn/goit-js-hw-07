9b4d351deea9e8c6bb4b0d8d4cc4804eff40cd29
polrmn <polrmn@gmail.com>
2022-12-21 11:04:59 +0200

    final 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDivRef = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMurkup(galleryItems);
+
+let lagreImageURL;
+
+
+galleryDivRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryDivRef.addEventListener("click", onGalleryImageClick);
+
+
+
+function createGalleryMurkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+        `
+    )
+    .join("");
+}
+function onGalleryImageClick(event) {
+    event.preventDefault(); 
+
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+    
+    lagreImageURL = event.target.dataset.source;
+
+    showLargeImage();
+
+}
+function showLargeImage() {
+    const instance = basicLightbox.create(`<img src="${lagreImageURL}">`);
+    instance.show();
+    closeLargeImage(instance);
+}
+function closeLargeImage(instance) {
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDivRef = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMurkup(galleryItems);
+
+galleryDivRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryDivRef.addEventListener("click", onGalleryImageClick);
+
+
+function createGalleryMurkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    )
+    .join("");
+}
+function onGalleryImageClick(event) {
+  event.preventDefault();
+    
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  
+  showLargeImagesSlider();
+}
+function showLargeImagesSlider() {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
